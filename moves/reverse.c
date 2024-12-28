18bdcc7384867c7f25cdab165bb80894a36d8e9a
LowReward <hellsingbarkbark@gmail.com>
2024-12-28 19:56:18 +0100

    rrb and rrr created

--- moves/reverse.c
+++ moves/reverse.c
@@ -6,7 +6,7 @@
 /*   By: fben-ham <fben-ham@student.42-angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 18:53:43 by fben-ham          #+#    #+#             */
-/*   Updated: 2024/12/28 19:52:32 by fben-ham         ###   ########.fr       */
+/*   Updated: 2024/12/28 19:55:52 by fben-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,33 @@ void rra(stack_a **head_a, stack_a **first_a)
         (*head_a) -> next = new_node;
         (*head_a) = (*head_a) -> next;
     }
+}
+
+void rrb(stack_b **head_b, stack_b **first_b)
+{
+    int tmp;
+    stack_b *tmp_first;
+    stack_b *new_node;
+
+    new_node = malloc(sizeof(stack_b));
+    if(list_len_b((*first_b)) > 1)
+    {
+        // save our first val and delete the node
+        tmp = (*first_b) -> val;
+        tmp_first = (*first_b) -> next;
+        free((*first_b));
+        (*first_b) = tmp_first;
+
+        // create a new node with the initial first val
+        new_node -> val = tmp;
+        new_node -> next = NULL;
+        (*head_b) -> next = new_node;
+        (*head_b) = (*head_b) -> next;
+    }
+}
+
+void rrr(stack_a **head_a, stack_a **first_a, stack_b **head_b, stack_b **first_b)
+{
+    rra(head_a, first_a);
+    rrb(head_b, first_b);
 }
