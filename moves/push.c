f5d33ac8400f85876b136d39b9301a6dcdc9ea33
NoctisLight <flush715@outlook.com>
2024-12-30 19:14:52 +0100

    Fixed the free part on push.c -> the last node werent NULLed on the previous node

--- moves/push.c
+++ moves/push.c
@@ -6,7 +6,7 @@
 /*   By: fben-ham <fben-ham@student.42-angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 13:44:04 by fben-ham          #+#    #+#             */
-/*   Updated: 2024/12/27 13:58:14 by fben-ham         ###   ########.fr       */
+/*   Updated: 2024/12/30 19:13:57 by fben-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,22 @@
 void pa(stack_a **head_a, stack_b **head_b, stack_b *first_b)
 {
     stack_a *new_node;
+    int i;
+    
     if(list_len_b(first_b))
     {
+        i = list_len_b(first_b) - 1;
         new_node = malloc(sizeof(stack_a));
         new_node -> val = (*head_b) -> val;
         new_node -> next = NULL;
+        while(i) // will get the lastnode-1 temporarly before deleting it
+        {
+            first_b = first_b -> next;
+            i--;
+        }
+        free((*head_b));
+        first_b -> next = NULL;
+        (*head_b) = first_b;
         (*head_a) -> next = new_node;
         *head_a = (*head_a) -> next;
     }
@@ -29,11 +40,22 @@ void pa(stack_a **head_a, stack_b **head_b, stack_b *first_b)
 void pb(stack_a **head_a, stack_b **head_b, stack_a *first_a)
 {
     stack_b *new_node;
+    int i;
+
     if(list_len_a(first_a))
     {
+        i = (list_len_a(first_a) - 2);
         new_node = malloc(sizeof(stack_b));
         new_node -> val = (*head_a) -> val;
         new_node -> next = NULL;
+        while(i) // will get the lastnode-1 temporarly before deleting it
+        {
+            first_a = first_a -> next;
+            i--;
+        }
+        free((*head_a));
+        first_a -> next = NULL;
+        (*head_a) = first_a;
         (*head_b) -> next = new_node;
         *head_b = (*head_b) -> next;
     }
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: fben-ham <fben-ham@student.42-angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 15:51:43 by fben-ham          #+#    #+#             */
-/*   Updated: 2024/12/28 19:51:43 by fben-ham         ###   ########.fr       */
+/*   Updated: 2024/12/30 16:57:28 by fben-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,15 @@ void check_ifok(char *all_in_one)
     stack_a *head_a;
     stack_a *first_a;
     stack_a *new_node;
+    stack_b *head_b;
+    stack_b *first_b;
     
     tab = ft_split(all_in_one, ' '); // split each numbers in a tab ready to be send to atoi
     head_a = malloc(sizeof(stack_a));
+    head_b = malloc(sizeof(stack_b));
     first_a = head_a;
+    first_b = head_b;
+    
     i = 0;
     while(tab[i]) // send each number to atoi  
     {   
@@ -49,6 +54,7 @@ void check_ifok(char *all_in_one)
         i++;
     }
     first_a = first_a->next; // skip the random 0 at the beginning of the list
+    //first_b = first_b->next;
     /*head = first_a; // reset head to first
 
     while(head) // simple while to print all of my nodes
@@ -60,12 +66,29 @@ void check_ifok(char *all_in_one)
     // THIS ZONE WILL BE USED TO CHECK IF OUR MOVES ARE WORKING / DO SOME TESTS
     //head = first_a; // reset head to first again to test X move
     ft_printf("\nTest Move :\n\n");
-    rra(&head_a, &first_a);
+    //head_a = first_a;
+    head_b = first_b;
+    ft_printf("\nList A :\n");
+    /*while(head_a)
+    {
+        printf("%d\n", head_a->val);
+        head_a = head_a->next;
+    }*/
+    ft_printf("\nList B :\n");
+    //head_a = first_a;
+    pb(&head_a, &head_b, first_a);
+    ft_printf("\nList A :\n");
     head_a = first_a;
     while(head_a)
     {
         printf("%d\n", head_a->val);
         head_a = head_a->next;
+    }
+    ft_printf("\nList B :\n");
+    while(head_b)
+    {
+        printf("%d\n", head_b->val);
+        head_b = head_b->next;
     }
 }
 
